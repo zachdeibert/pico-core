21b91e08571fa0fa8ff19ebb82f20800e4a91a73
Zach Deibert <zachdeibert@gmail.com>
2024-02-12 21:04:02 -0600

    Add compiler warning config

--- init.cmake
+++ init.cmake
@@ -12,8 +12,20 @@ endif()
 
 include(${CMAKE_CURRENT_LIST_DIR}/external/pico-sdk/pico_sdk_init.cmake)
 
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/cmake)
+list(APPEND CMAKE_MODULE_PATH
+    ${CMAKE_CURRENT_LIST_DIR}/../core/cmake
+    ${CMAKE_CURRENT_LIST_DIR}/cmake
+)
 include(pico_add_flash_target)
 
 set(CMAKE_C_STANDARD 11)
 set(CMAKE_CXX_STANDARD 17)
+include(Wall)
+add_compile_definitions(
+    LIB_CMSIS_CORE=0
+)
+if (NOT MSVC)
+    add_compile_options(
+        -Wdouble-promotion
+    )
+endif()
